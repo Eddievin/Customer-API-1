33588ba2f943e9065e74bf8dd40947d54d64f33b
Eddievin <thokerunga200@gmail.com>
2021-04-15 13:17:09 +0300

    updated code

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req,res, next)=>{
 });
 
 app.get("/", (req, res, next) => {
-  res.send("API is working properly");
+  res.send("API is working properly now");
 });
 app.get("/customers", customerCtrl.getCustomers);
 app.get("/customer/:customerId", customerCtrl.getCustomerDetails);
